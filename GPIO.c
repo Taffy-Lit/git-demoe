d6c74476ddd967fdb90f9df5985897f5165aabc6
Taffy-Lit <wjh2890617474@163.com>
2026-06-27 11:46:32 +0800

    添加终止程序

--- GPIO.c
+++ GPIO.c
@@ -6,3 +6,10 @@ if (Read_GPIO_Pin(GPIO_PIN_0) == GPIO_PIN_SET) {
     // Pin is low
     GPIO_WritePin(GPIO_PIN_1, GPIO_PIN_RESET); // Set another pin low
 }
+
+uint8_t count ;
+count++;
+if(count == 500)
+{
+    GPIO_DeInit();
+}
